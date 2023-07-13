51489ef0b191aedf6ea95287e8afae1709bc9187
vishalkondle45 <vishal.kondle@gmail.com>
2023-07-13 14:35:26 +0530

    mantine setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { MantineProvider } from "@mantine/core";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <App />
+  </MantineProvider>
+);
